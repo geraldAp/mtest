b2603c882e95458c767553829571c5d1f911be6e
geraldAp <117451278+geraldAp@users.noreply.github.com>
2023-05-19 16:05:31 +0100

    second

--- src/components/showcase.jsx
+++ src/components/showcase.jsx
@@ -5,16 +5,16 @@ const Showcase = () => {
     <>
       <section id="Showcase">
         <div
-          className="w-full md:relative h-screen bg-no-repeat bg-cover bg-left-top flex justify-center items-center md:block md:bg-center  "
+          className="w-full md:relative h-[60vh] md:h-screen bg-no-repeat bg-cover bg-left-top flex justify-center items-center md:block md:bg-center  "
           style={{
             backgroundImage: 'url("/IMAGES/showcase-background.jpg")',
           }}
         >
           <div className="md:absolute font-jah text-center lg:text-left text-[#FFFFFF] md:left-[147px]  md:top-[18%] lg:top-[21%]">
             <p className=" uppercase tracking-wider text-[6rem] md:text-[10rem] lg:font-medium lg:text-[12rem]  ">
-             <span className="block  h-32  md:h-40 ">  The best</span>
-             <span className="block h-32 md:h-40">  Delicious</span>
-             <span className="block h-32 text-[#FFB500]  md:h-36 ">  food</span>
+             <span className="block  h-24  md:h-40 ">  The best</span>
+             <span className="block h-24 md:h-40">  Delicious</span>
+             <span className="block h-24 text-[#FFB500]  md:h-36 ">  food</span>
             </p>
            
           </div>
